6f3b384aa9b16c72148d0dd1f52bf14487bedf42
FlavioKRdL <flaviokaneichi@gmail.com>
2024-03-06 18:19:54 -0300

    Praticandos e organização
    Terminados os praticandos da aula do dia 22

---
+++ 22_02_2024/2024/eder/ola.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo("<h1>Minha segunda página em PHP!!</h1>");
+        echo("<br><br>");
+        echo("<a href='https://localhost/aula3/index.php'>Voltar</a>");
+    ?>
+</body>
+</html>
---
+++ 22_02_2024/aula3/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+</head>
+<body>
+    
+    <?php
+        echo("<h1>Nome: Flávio Kaneichi Riberto de Lima<h1>");
+        echo("<br><br>");
+        echo("<a href='hello.php'>Hello World!</a>");
+        echo("<br>");
+        echo("<a href='https://localhost/2024/eder'>2024/eder</a>");
+    ?>
+    
+
+</body>
+</html>
