423181deee559be0daa69444ddbe7435188fb77b
FlavioKRdL <flaviokaneichi@gmail.com>
2024-03-08 19:44:28 -0300

    Finalizados os praticandos do dia 29/02/2024

---
+++ 29_02_2024/Aula4/calcmedia-post.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Praticando: Calculadora de Média</h1>
+    <br><br><br>
+
+    <?php 
+        print_r($_POST);
+        echo("<br><br>");
+
+        $nota1 = filter_input(INPUT_POST, "nota1", FILTER_SANITIZE_NUMBER_FLOAT);
+        $nota2 = filter_input(INPUT_POST, "nota2", FILTER_SANITIZE_NUMBER_FLOAT);
+        $nota3 = filter_input(INPUT_POST, "nota3", FILTER_SANITIZE_NUMBER_FLOAT);
+
+        $media = ($nota1+$nota2+$nota3)/3;
+        echo("Média sem tratamento: ".var_dump($media));
+        echo("<br>");
+        $media_string = number_format((float)$media, 2, '.', '');
+        echo("Média com tratamento(string): ".var_dump($media_string));
+        echo("<br>");
+        $media = floatval($media_string);
+        echo("Média após tratamento e reconversão em float: ".var_dump($media));
+        echo("<br>");
+
+        echo("<p>O/A aluno(a) com notas $nota1, $nota2, e $nota3 tem média igual a $media</p>");
+        echo("<br>");
+
+        switch($media){
+            case $media<4.0:
+                $estado = "reprovado";
+                $cor = "red";
+                break;
+
+            case $media<6.0:
+                $estado = "recuperação";
+                $cor = "yellow";
+                break;
+
+            case $media>=6.0:
+                $estado = "aprovado";
+                $cor = "green";
+        }
+    ?>
+
+    <style>
+        .p1{
+            color: <?php echo($cor); ?>;
+        }
+    </style>
+
+    <?php 
+        echo("<p>Com essa media, o aluno está <span class=p1>$estado</span></p>");
+    ?>
+</body>
+</html>
--- 29_02_2024/Aula4/destino-get.php
+++ 29_02_2024/Aula4/destino-get.php
@@ -4,12 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        .sect1{
+            width : 50vw ;
+            height: 12vh;
+            display:flex;
+            justify-content:space-between;
+        }
+        .sq-red{
+            background-color: red;
+            height: 10vh;
+            width: 10vh;
+        }
+        .sq-green{
+            background-color: green;
+            height: 10vh;
+            width: 10vh;
+        }
+        .sq-blue{
+            background-color: blue;
+            height: 10vh;
+            width: 10vh;
+        }
+    </style>
 </head>
 <body>
     <h1>Destino</h1>
     <hr>
 
     <?php
+
+        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
+            $url = "https://";   
+        else  
+            $url = "http://";   
+        // Append the host(domain name, ip) to the URL.   
+        $url.= $_SERVER['HTTP_HOST'];
+        echo $url; 
+        echo "<br>";
+
+        // Append the requested resource location to the URL   
+        $url.= $_SERVER['REQUEST_URI'];    
+
+        echo $url;  
+
+        echo "<br><br>";
         print_r($_GET);
         echo("<br><br>");
         /*
@@ -27,7 +67,20 @@
         $email = filter_input(INPUT_GET, "email", FILTER_SANITIZE_EMAIL);
         $cor = filter_input(INPUT_GET,"cor", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        echo("<p>Nome informado: $nome <br>E-mail: $email</p>");
+        if(!empty($nome)){
+            echo("<p>Nome informado: $nome </p>");
+            echo(var_dump($nome));
+        }
+        if(!empty($email)){
+            echo("<p>E-mail informado: $email</p>");
+        }
+
+        if($nome=="Jose Louro"){
+            $nome_url="Jose%20Louro";
+        }
+        else{
+            $nome_url=$nome;
+        }
     ?>
 
     <style>
@@ -38,14 +91,26 @@
 
     <br><br>
 
-    <a href="https://localhost/aula5/destino-get.php?nome=Flavio&email=flavio@email.com&cor=blue">[Nome = Flavio, email = flavio@email.com, cor = blue]</a>
+    <a href="https://localhost/aula4/destino-get.php?nome=Flavio&email=flavio@email.com&cor=blue">[Nome = Flavio, email = flavio@email.com, cor = blue]</a>
     <br>
-    <a href="https://localhost/aula5/destino-get.php?nome=Jose%20Louro&email=jose@email.com&cor=green">[Nome = Jose Louro, email = jose@email.com, cor = green]</a>
-    <br>
+    <a href="https://localhost/aula4/destino-get.php?nome=Jose%20Louro&email=jose@email.com&cor=green">[Nome = Jose Louro, email = jose@email.com, cor = green]</a>
+    <br><br>
 
-    
+    <section class="sect1">
+        <a href=<?php echo("https://localhost/aula4/destino-get.php?nome=$nome_url&email=$email&cor=red"); ?>>
+            <div class="sq-red"></div>
+        </a>
+
+        <a href=<?php echo("https://localhost/aula4/destino-get.php?nome=$nome_url&email=$email&cor=green"); ?>>
+            <div class="sq-green"></div>
+        </a>
+
+        <a href=<?php echo("https://localhost/aula4/destino-get.php?nome=$nome_url&email=$email&cor=blue"); ?>>
+            <div class="sq-blue"></div>
+        </a>
+    </section>
 
     <br>
-    <a href="https://localhost/aula5/destino-get.php">Limpar tudo</a>
+    <a href="https://localhost/aula4/destino-get.php">Limpar tudo</a>
 </body>
 </html>
